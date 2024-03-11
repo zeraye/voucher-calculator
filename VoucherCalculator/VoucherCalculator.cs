75eea6572ad5e7033f1095c060994472565141d5
zeraye <zeraye@protonmail.com>
2024-03-11 11:41:34 +0100

    feat: implement CalculateDiscount for vouchers

---
+++ VoucherCalculator/VoucherCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace VoucherCalculator
+{
+    public class VoucherCalculator
+    {
+        private Dictionary<string, double> vouchers;
+
+        public VoucherCalculator()
+        {
+            vouchers = new Dictionary<string, double>();
+            vouchers["SAVE10NOW"] = 10.0;
+        }
+
+        public double CalculateDiscount(double basePrice, string voucher)
+        {
+            if (vouchers.ContainsKey(voucher))
+            {
+                return basePrice * (100.0 - vouchers[voucher])/100.0;
+            }
+
+            throw new ArgumentException("Invalid discount code");
+        }
+
+    }
+}
